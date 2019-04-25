de2093d9035d2f96dec8f39d5a348db255f981b3
Anthony Yarbrough <asyarb@hawaii.edu>
2019-04-24 22:46:28 -1000

    feat: initial electron support

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ThemeProvider } from 'emotion-theming'
-import styled from '@emotion/styled'
+import styled from '@emotion/styled/macro'
 
 import { GlobalStyle } from './GlobalStyle'
 import { theme } from '../theme'
